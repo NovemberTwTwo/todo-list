04e9695e7ffb552db320b766877cd1578d6475f0
DaHyunOhe <peelsob4d@gmail.com>
2023-10-09 21:00:27 +0900

    fix : fixed TextBox unit expressions

--- src/components/UI/TextBox.ts
+++ src/components/UI/TextBox.ts
@@ -1,7 +1,16 @@
 import { styled } from 'styled-components';
 
-const TextBox = styled.p<{ size?: number; color?: string }>`
-  font-size: '${(props) => (props.size ? props.size : 1)}rem';
+const TextBox = styled.p<{
+  size?: number;
+  color?: string;
+  weight?: number;
+  marginTop?: number;
+  marginBottom?: number;
+}>`
+  margin-top: ${(props) => (props.marginTop ? props.marginTop : 0)};
+  margin-bottom: ${(props) => (props.marginBottom ? props.marginBottom : 0)};
+  font-size: calc(${(props) => (props.size ? props.size : 1)} * 1px);
+  font-weight: ${(props) => (props.weight ? props.weight : 400)};
   color: ${(props) =>
     props.color ? props.color : props.theme.colors.fontMain};
 `;
