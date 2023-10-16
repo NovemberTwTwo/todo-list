36c8a516cf1b0b162cb03619b7b6fdb72ca43603
DaHyunOhe <peelsob4d@gmail.com>
2023-10-16 20:04:48 +0900

    feat: Added Button styling

--- src/components/UI/Button.ts
+++ src/components/UI/Button.ts
@@ -1,5 +1,55 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { spacing } from './mixin';
+import { BoxSizing, BoxSpacing, FontSizing } from './types/types';
 
-const Button = styled.button``;
+interface ButtonProps extends BoxSizing, BoxSpacing, FontSizing {
+  $backgroundColor?: string;
+  $borderColor?: string;
+  $borderRadius?: number;
+  $fontColor?: string;
+}
+
+const Button = styled.button<ButtonProps>`
+  width: ${({ $width }) => ($width ? $width : '84px')};
+  height: ${({ $height }) => ($height ? $height : 'auto')};
+  background-color: ${(props) =>
+    props.$backgroundColor ? props.$backgroundColor : props.theme.colors.brand};
+  color: ${(props) =>
+    props.$fontColor ? props.$fontColor : props.theme.colors.main};
+  font-family: 'Pretendard';
+  border: 0;
+
+  ${({ $borderColor }) =>
+    $borderColor &&
+    css`
+      border: solid 1px ${$borderColor};
+    `}
+
+  ${({ $borderRadius }) =>
+    $borderRadius &&
+    css`
+      border-radius: ${$borderRadius}px;
+    `}
+  
+  ${({ $fontSize }) =>
+    $fontSize &&
+    css`
+      font-size: ${$fontSize};
+    `}
+
+  ${({ $lineHeight }) =>
+    $lineHeight &&
+    css`
+      line-height: ${$lineHeight};
+    `}
+
+  ${({ $fontWeight }) =>
+    $fontWeight &&
+    css`
+      font-weight: ${$fontWeight};
+    `}
+
+  ${spacing}
+`;
 
 export default Button;
